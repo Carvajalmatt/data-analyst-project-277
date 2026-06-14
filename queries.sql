418beffd9016b98e092f4e6cb0355bdbc4948f44
Carvajalmatt <carvajalmatt@hotmail.com>
2026-06-14 14:56:22 -0500

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,4 @@
+-- Cuenta el número total de clientes registrados en la tabla customers
+-- El resultado se muestra en una columna llamada customers_count
+SELECT COUNT(*) AS customers_count
+FROM customers;
